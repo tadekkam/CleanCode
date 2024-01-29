ad1333f4773f313b0e82c25887dca55a9f3e7b2d
Tadeusz Kamyszek <tkamyszek@op.pl>
2024-01-29 21:39:14 +0100

    Refactoring

---
+++ src/main/java/org/example/VehicleEngine.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class VehicleEngine extends Vehicle {
+
+    private boolean engineStarted;
+
+    protected void startEngine() {
+        if (!engineStarted) {
+            engineStarted = true;
+        }
+    }
+
+    protected void stopEngine() {
+        if (engineStarted) {
+            engineStarted = false;
+        }
+    }
+
+    public boolean isEngineStarted() {
+        return engineStarted;
+    }
+
+    public void setEngineStarted(boolean engineStarted) {
+        this.engineStarted = engineStarted;
+    }
+
+}
