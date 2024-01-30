8c144410c4bc2f3dbf0d473fe171d2ae05926fa6
Tadeusz Kamyszek <tkamyszek@op.pl>
2024-01-30 09:01:42 +0100

    Refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 0; i <= 100; i++) {
-            if (Modulo.mod(i,3) && Modulo.mod(i,5)) {
-                System.out.println("FizzBuzz");
-            } else if (Modulo.mod(i,3)) {
-                System.out.println("Fizz");
-            } else if (Modulo.mod(i,5)) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
+            Modulo.fizzBuzz(i);
         }
     }
 }
--- src/main/java/org/example/Modulo.java
+++ src/main/java/org/example/Modulo.java
@@ -6,4 +6,16 @@ public class Modulo {
         }
         return false;
     }
+    public static void fizzBuzz(int argA) {
+        if (mod(argA,3) && Modulo.mod(argA,5)) {
+            System.out.println("FizzBuzz");
+        } else if (mod(argA,3)) {
+            System.out.println("Fizz");
+        } else if (mod(argA,5)) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(argA);
+        }
+    }
+
 }
